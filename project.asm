831ae8c9f1cff4124c2b6071435ec75c86577ab0
Sovego <egor.sofronov03@gmail.com>
2022-04-23 02:50:10 +0700

    add some comments to assembler code

--- project.asm
+++ project.asm
@@ -2,12 +2,17 @@ asect 0x00
 
 
 # 0xF1 - чип для получения поля передает массив за 9 обращений
-# 0xF2 - чип хода на вход пока хз что
+# 0xF2 - чип хода на вход номер ячейки от 0 до 8
 # 0xF3 - чип передающий инфу о текущем игроке 1 нолик
 ### ожидание своего хода ###
+#ldi r0,0xF3 для тестирования
+#ldi r1,1
+#st r0,r1
 wait: 
 while
-stays nz
+	ldi r0,0
+	tst r0
+stays z
 	ldi r0,0xF3
 	ld r0,r0
 	if 
@@ -16,22 +21,25 @@ stays nz
 	is z
 		jsr maketurn
 	fi
-wend
+wend	
+halt
 ### совершаем ход ###	
-maketurn: 
+maketurn:
+# записываем поле в массив 
 ldi r0,0
 while
 	
 	ldi r1,8
 	cmp r0,r1
 stays nz
-	ldi r1,field_mas
+	ldi r1,field_mas 
 	add r0,r1
 	ldi r2,0xF1
 	ld r2,r2
 	st r1,r2
 	inc r0
 wend
+# ищем первое попавшееся место
 ldi r0,0
 while 
 	ldi r1,8
@@ -47,10 +55,14 @@ stays nz
 		st r2,r0
 		break
 	fi
-	inc r0
+	inc r0	
 wend
-
+#ldi r0,0xF3 для тестирования
+#ldi r1,0
+#st r0,r1
 rts
+jsr wait # потому что rts не работает
+
 
 
 field_mas: ds 9
